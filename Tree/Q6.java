38f304e0b90afc5e912f30208c52ee3af1e53104
Pedro Affonso <pedroaffonsosilvamarques@gmail.com>
2023-01-06 20:38:48 -0300

    fixing identation

--- Tree/Q6.java
+++ Tree/Q6.java
@@ -1,18 +1,19 @@
 package Tree;
 
 import Tree.lib.Tree;
-public class Q6 {
-    public static void main(String args[]){
-     //a função isHeap está no arquivo da Tree 
-     Tree<Integer> Tree = new Tree <Integer>();
-     Tree.insertBT(Tree.getRoot(), 1);
-     Tree.insertRight(Tree.getRoot(),0);
-     Tree.insertLeft(Tree.getRoot(), -1);
-     
-     System.out.println(Tree.getRoot().getKey());
-     System.out.println(Tree.getRoot().getLeft().getKey());
-     System.out.println(Tree.getRoot().getRight().getKey());
 
-     System.out.println(Tree.isHeap());
-    }
+public class Q6 {
+  public static void main(String args[]) {
+    // a função isHeap está no arquivo da Tree
+    Tree<Integer> Tree = new Tree<Integer>();
+    Tree.insertBT(Tree.getRoot(), 1);
+    Tree.insertRight(Tree.getRoot(), 0);
+    Tree.insertLeft(Tree.getRoot(), -1);
+
+    System.out.println(Tree.getRoot().getKey());
+    System.out.println(Tree.getRoot().getLeft().getKey());
+    System.out.println(Tree.getRoot().getRight().getKey());
+
+    System.out.println(Tree.isHeap());
+  }
 }
